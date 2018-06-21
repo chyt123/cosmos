2c070e48eb4a3090d8593a62e5eb8e2fa8ccd58b
Siheng Chen <sihengc@sihengc-a02.vmware.com>
2018-06-21 18:44:44 +0800

    csh: lc798 AC

--- coding_everyday/lc798/csh.py
+++ coding_everyday/lc798/csh.py
@@ -8,19 +8,31 @@ class Solution(object):
         r = list()
         for i in range(0, l):
             if A[i] <= i: # can be move to left
-                r.append([0, True])
-                r.append([i - A[i], False])
+                r.append([0, False])
+                r.append([i - A[i], True])
 
             # every num can be move to right
             move_right_start = i + 1
             move_right_end = min(l - A[i] + i, l-1)
             if move_right_start <= move_right_end:
-                r.append([move_right_start, True])
-                r.append([move_right_end, False])
+                r.append([move_right_start, False])
+                r.append([move_right_end, True])
 
-        for i in r:
-            print i
-        return
+        r.sort()
+        _max = 0
+        p = 0
+        K = 0
+
+        for i, e in enumerate(r):
+            if not e[1]:
+                p += 1
+                if p > _max:
+                    _max = p
+                    K = e[0]
+            else:
+                p -= 1
+
+        return K
 
 
 if __name__ == "__main__":
