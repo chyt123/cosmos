264ab4e38df291b7e2aa8ce3a620c1409fd005fc
陈思恒 <chensh@chensihengdeMacBook-Pro.local>
2018-06-29 00:23:53 +0800

    csh 797 ac

--- coding_everyday/lc797/csh.py
+++ coding_everyday/lc797/csh.py
@@ -1,30 +1,24 @@
+import copy
+
 class Solution(object):
 
+    def dfs(self, node, path, graph, result):
+        if node == len(graph) - 1:
+            result.append(path)
+        for i in graph[node]:
+            self.dfs(i, path + [i], graph, result)
+        return result
+            
     def allPathsSourceTarget(self, graph):
-        node_stack = list()
-        result = list()
-        tmp_result = list()
-        node_stack.append(0)
-        while len(node_stack):
-            top = node_stack.pop()
-            tmp_result.append(top)
-            print tmp_result, result
-            if len(graph[top]) > 1:
-                tmp_result.append(-1)
-            elif not len(graph[top]):
-                if top == len(graph) - 1:  # found a path
-                    result.append(tmp_result)
-                else:  # found a dead end
-                    pass
-                while tmp_result[-1] >= 0 or node_stack[-1] not in graph[tmp_result[-2]]:
-                    tmp_result.pop()
-            for i in graph[top]:
-                node_stack.append(i)
-
+        result = []
+        return self.dfs(0, [0], graph, result)
 
 if __name__ == "__main__":
     sol = Solution()
-    # A = [2, 3, 1, 4, 0]
-    A = [[1,2],[3,4],[3],[],[5],[]]
+    # A = [[1,2], [3], [3], []]
+    # A = [[1,2],[3,4],[3],[],[5],[]]
+    A = [[1,2],[3,5],[3],[4],[6],[6],[]]
+    # A = [[4,3,1],[3,2,4],[3],[4],[]]
+    # A = [[3,1],[4,6,7,2,5],[4,6,3],[6,4],[7,6,5],[6],[7],[]]
     print sol.allPathsSourceTarget(A)
 
