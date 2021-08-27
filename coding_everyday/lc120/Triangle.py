e19d0cd812d1eb0e39cb4391e93a535e94a626c1
Chen <chensh1992@hotmail.com>
2021-08-27 12:43:26 +0800

    21/08/27

---
+++ coding_everyday/lc120/Triangle.py
@@ -0,0 +1,32 @@
+import collections
+from typing import List, Optional
+from util import (
+    ListNode, lc_list2singlelinkedlist, lc_singlelinkedlist2list,
+    TreeNode, lc_tree2list, lc_list2tree
+)
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        for i in range(1, n):
+            for j in range(len(triangle[i])):
+                if j == 0:
+                    triangle[i][j] += triangle[i - 1][j]
+                elif j == len(triangle[i]) - 1:
+                    triangle[i][j] += triangle[i - 1][j - 1]
+                else:
+                    triangle[i][j] += min(triangle[i - 1][j], triangle[i - 1][j - 1])
+        return min(triangle[-1])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    test_cases = [
+        [[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]],
+        [[-10]],
+    ]
+    for i in test_cases:
+        result = sol.minimumTotal(i)
+        print(result)
+
